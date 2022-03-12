fe5752c15c4b3794e1b0ccea498d91165e38d38f
loakesh <72959109+loakesh@users.noreply.github.com>
2022-03-12 12:18:21 +0530

    CSS and JS files

---
+++ static/JS/script.js
@@ -0,0 +1,47 @@
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+
+$("#reg_form").on("submit",function(){
+   var values = $(this).serializeArray();
+   
+   var udata = {"name":values[0]["value"],"password":values[1]["value"],"email":values[2]["value"],}
+  
+   $.ajax({
+      url:"http://localhost:5000/create_user",
+      type:"POST",
+      data:JSON.stringify(udata),
+      dataType:"JSON",
+      contentType: "application/json; charset=utf-8",
+      success:function(data){
+         alert("Tadaa!!!!")
+         console.log(data)
+        
+      }
+      
+
+      
+   })
+
+
+})
+
+$("#login_form").on("submit",function(){
+   var values = $(this).serializeArray();
+   var udata = {"name":values[0]["value"],"password":values[1]["value"]}
+   $.ajax({
+      url:"http://localhost:5000/login_user",
+      type:"POST",
+      data:JSON.stringify(udata),
+      dataType:"JSON",
+      contentType: "application/json; charset=utf-8",
+      success:function(data){
+         alert(data["status"])
+         
+        
+      }
+      
+   })
+})
